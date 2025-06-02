864dd47bbd9b7e0d0796eb80f32075858906db2b
jsalinasninja <jsalinas@ninjaexcel.com>
2025-06-01 20:28:45 -0500

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,12 @@
 export const environment = {
-    production: true,
-    apiUrl: 'https://api.boxexpress.com/api'
-  };
-  
+    production: false,
+    apiUrl: 'https://api.boxexpress.com/api',
+    auth: '/Auth/login',
+    defaultValues: {
+      cityId: 1,
+      countryId: 1,
+      balance: 0,
+      pickupAddress: 'BodegajeBoxExpress'
+    }
+};
+
