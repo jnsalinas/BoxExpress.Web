5aa7acc2e5fa06b008a8f43e99ef6a8b03203e04
jsalinasninja <jsalinas@ninjaexcel.com>
2025-07-25 14:05:33 -0500

    remove nx

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,8 +1,8 @@
 export const environment = {
     production: false,
-    // apiUrl: 'https://boxexpress-api.onrender.com/api',
-    apiUrl: 'http://localhost:5143/api', //run
-    // apiUrl: 'https://localhost:7001/api', //debug 
+    apiUrl: 'https://boxexpress-api.onrender.com/api',
+    //apiUrl: 'http://localhost:5143/api', //run
+    //  apiUrl: 'https://localhost:7001/api', //debug 
     auth: '/Auth/login',
     defaultValues: {
       cityId: 1,
