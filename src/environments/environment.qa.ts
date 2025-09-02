96aaeb1e5bd4df095efe8f3823a20f09d213dfd8
jsalinasninja <jsalinas@ninjaexcel.com>
2025-09-02 15:36:40 -0500

    chore(qa): agregar environment qa, config angular y workflow build:qa

---
+++ src/environments/environment.qa.ts
@@ -0,0 +1,13 @@
+export const environment = {
+    production: false,
+    apiUrl: 'https://boxexpress-service.azurewebsites.net/api',
+    auth: '/Auth/login',
+    defaultValues: {
+      cityId: 1,
+      countryId: 1,
+      balance: 0,
+      pickupAddress: 'BodegajeBoxExpress'
+    }
+};
+
+
