a8399391fbc08975b82c2309809ff1e05d0e59d4
jsalinasninja <jsalinas@ninjaexcel.com>
2025-09-10 18:49:26 -0500

    fix styles

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,8 @@
 export const environment = {
     production: false,
-    // apiUrl: 'https://boxexpress-api.onrender.com/api',
-    apiUrl: 'http://localhost:5143/api', //run
+    apiUrl: 'https://boxexpress-api.onrender.com/api',
+    // apiUrl: 'https://boxexpress-service.azurewebsites.net/api',
+    // apiUrl: 'http://localhost:5143/api', //run
     // apiUrl: 'https://localhost:7001/api', //debug 
     auth: '/Auth/login',
     defaultValues: {
